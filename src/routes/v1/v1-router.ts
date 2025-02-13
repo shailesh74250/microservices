5e4783799431c123220cf8291005cda730f31667
unknown <smourya7425@gmail.com>
2025-02-14 00:39:21 +0530

    wip

--- src/routes/v1/v1-router.ts
+++ src/routes/v1/v1-router.ts
@@ -3,7 +3,6 @@ import testRouter from '../../modules/test-module/test-router';
 
 const v1 = Router();
 
-v1.use('/auth', testRouter);
-v1.use('/user', testRouter);
+v1.use('/test', testRouter);
 
 export default v1;
